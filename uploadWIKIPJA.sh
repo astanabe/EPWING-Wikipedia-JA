649801e39a08287d5f3e66abcafc7ed11b5d5d19
astanabe <akifumi.tanabe@gmail.com>
2024-08-12 07:53:50 +0900

    The asset file upload script has been updated.

--- uploadWIKIPJA.sh
+++ uploadWIKIPJA.sh
@@ -2,6 +2,25 @@ read -p 'input github token: ' github_token
 user_name="astanabe"
 repo_name="EPWING-Wikipedia-JA"
 tag_name=`cat tag_name.txt`
+DATE=`cat date.txt`
+
+# Get release list
+response=`curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${github_token}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/${user_name}/${repo_name}/releases`
+
+# Check tag and create release if not exist
+if test `echo ${response} | jq '.[] | .tag_name' | grep -c "${tag_name}"` -eq 0; then
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${github_token}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/${user_name}/${repo_name}/releases \
+  -d "{\"tag_name\":\"${tag_name}\",\"name\":\"${repo_name} ${tag_name}\",\"body\":\"The JIS X 4081 (EPWING-compatible) format electronic dictionary generated from dump data of ja.wikipedia.org and converter script (version of ${DATE}).\",\"draft\":false,\"prerelease\":true,\"generate_release_notes\":false,\"make_latest\":\"true\"}"
+fi
 
 # Get a release by tag name
 response=`curl -L \
